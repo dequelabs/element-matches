b6b5a3831a50f858e7c7a164f9cefd9abd19c22c
David Sturley <david.sturley@deque.com>
2015-06-08 17:08:56 -0400

    Update dev dependencies; remove baked in blanket_mocha adapter.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			files: ['lib/**/*', 'test/**/*'],
-			tasks: ['concat']
+			tasks: ['test']
 		},
 		connect: {
 			test: {
